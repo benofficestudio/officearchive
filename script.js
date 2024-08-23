1e88c0df0597aa72a67b20156511deed2b6cba9b
benofficestudio <103466026+benofficestudio@users.noreply.github.com>
2024-08-23 02:12:59 +0000

    added flexbox image layout.

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+
+// Get the h1 element and link
+var titleLink = document.getElementById('titleLink');
+
+// Add event listener for mouse hover
+titleLink.addEventListener('mouseover', function() {
+  // Change the text to "office archive" on hover
+  titleLink.textContent = 'office repository';
+  // Update the link to the "office archive" event
+  titleLink.href = "https://benofficestudio.github.io/officerepository/";
+});
+
+// Add event listener for mouseout
+titleLink.addEventListener('mouseout', function() {
+  // Change the text back to "office repository" when not hovering
+  titleLink.textContent = 'office archive';
+  // Update the link back to the "office repository" (same link for simplicity)
+  titleLink.href = "https://benofficestudio.github.io/officerepository/";
+});
